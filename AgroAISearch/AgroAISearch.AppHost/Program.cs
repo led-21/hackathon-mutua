59d12d961fa4e0de2cfc324cbb63696e2da11eb8
Adriano <adriano.godoy@live.com>
2025-04-28 16:01:28 -0400

    add Aspire.Hosting.NodeJs for FrontEnd

--- AgroAISearch/AgroAISearch.AppHost/Program.cs
+++ AgroAISearch/AgroAISearch.AppHost/Program.cs
@@ -5,9 +5,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var backend = builder.AddProject<Projects.hackaton_mutua_agro>("backend");
 
-var frontend = builder.AddDockerfile("frontend", "..\\FrontEnd\\src")
-    .WithReference(backend)
-    .WithHttpEndpoint(port:8503, targetPort:8501)
+var frontend = builder.AddNpmApp("frontend", "..\\FrontEnd\\react")
+    .WithEnvironment("PORT","3001")
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
